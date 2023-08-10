294532ebf0eb5f5c38c7500ea1942fbf4a961e35
AbelBerges <abel.berges@gmail.com>
2023-08-10 09:14:04 -0300

    Subo el último proyecto

---
+++ OtroProyecto/src/otroproyecto/OtroProyecto.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package otroproyecto;
+
+/**
+ *
+ * @author kalema
+ */
+public class OtroProyecto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Este es el último que voy a crear para probar el agragado y la actialización
+    }
+    
+}
